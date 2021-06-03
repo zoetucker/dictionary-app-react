3c1698d02e9b430f35824f65ac859eb1080a4cb7
zoetucker <78665113+zoetucker@users.noreply.github.com>
2021-06-03 15:13:29 -0400

    imported axios and dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState();
 
+  // Dictionary api documentation > https://api.dictionaryapi.dev/api/v2/entries/en_US/hello
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleSearch);
+  }
+
+  function handleSearch(response) {
+    console.log(response.data[0]);
   }
 
   function updateWord(event) {
-    console.log(event);
     setKeyword(event.target.value);
   }
 
