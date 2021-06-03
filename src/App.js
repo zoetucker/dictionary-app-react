32289dec02a166795f3924529fd99b7f7ced5704
zoetucker <78665113+zoetucker@users.noreply.github.com>
2021-06-03 11:09:29 -0400

    added sub header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">Word Finder</header>
+      <h2>Which word are you looking for today?</h2>
     </div>
   );
 }
