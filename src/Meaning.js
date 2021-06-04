ae729236d58bb774bd2cb4c104d266ea9d7c3b82
zoetucker <78665113+zoetucker@users.noreply.github.com>
2021-06-03 22:07:48 -0400

    remove <br />

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,7 +9,7 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>{definition.definition}</p>
-            <br />
+
             <em>{definition.example}</em>
           </div>
         );
