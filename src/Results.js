905195923ed765f43e773bfcb25f984980b05d7a
zoetucker <78665113+zoetucker@users.noreply.github.com>
2021-06-03 22:05:35 -0400

    created meaning component

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Results(props) {
   console.log(props.results);
   if (props.results) {
     return (
       <div className="Results">
-        <h3>Here are results!</h3>
+        <h3>{props.results.word}</h3>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
       </div>
     );
   } else {
