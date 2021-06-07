29c212b01352e7f4fbac89b9e36933977764fa6b
zoetucker <78665113+zoetucker@users.noreply.github.com>
2021-06-07 10:44:34 -0400

    add heading for synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,11 +3,16 @@ import React from "react";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <ul className="Synonyms">
-        {props.synonyms.map(function (synonyms, index) {
-          return <li key={index}>{synonyms}</li>;
-        })}
-      </ul>
+      <div className="Synonyms">
+        <p>
+          <strong>Synonyms: </strong>
+        </p>
+        <ul className="Synonyms">
+          {props.synonyms.map(function (synonyms, index) {
+            return <li key={index}>{synonyms}</li>;
+          })}
+        </ul>
+      </div>
     );
   } else {
     return null;
