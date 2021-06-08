d2e3f24f22d5d54f91110cc66c081fc988ce2a1b
zoetucker <78665113+zoetucker@users.noreply.github.com>
2021-06-08 16:27:54 -0400

    na

--- src/Results.js
+++ src/Results.js
@@ -7,6 +7,7 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h3>{props.results.word}</h3>
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
