b76a13c5edf2d5f269cef8362111f2d59e44b628
zoetucker <78665113+zoetucker@users.noreply.github.com>
2021-06-09 19:49:20 -0400

    added photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState();
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   // Dictionary api documentation > https://api.dictionaryapi.dev/api/v2/entries/en_US/hello
 
@@ -14,10 +16,23 @@ export default function Dictionary() {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleSearch);
+
+    let pexelApikey =
+      "563492ad6f917000010000014f6ea90ee0694b918e756adba3412b40";
+    let pexelApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+
+    axios
+      .get(pexelApiUrl, {
+        headers: { Authorization: `Bearer ${pexelApikey}` },
+      })
+      .then(handlePexelResponse);
+  }
+
+  function handlePexelResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function handleSearch(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
 
@@ -51,6 +66,7 @@ export default function Dictionary() {
       </section>
 
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
